66cf390a77c29ceb34854481afb061ebd39f0363
Eugene Korpan <eugene@active-bridge.com>
2015-03-03 20:04:10 +0200

    Changed http status codes to corresponding symbols

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,28 +5,28 @@ class EventsController < ApplicationController
     json = ActiveModel::ArraySerializer.new(Event.order('rating desc'),
                                            each_serializer: EventSerializer,
                                            root: nil)
-    render json: json, status: 200
+    render json: json, status: :ok
   end
 
   def create
     event = Event.new(event_params)
     if event.save
-      render json: EventSerializer.new(event), status: 200
+      render json: EventSerializer.new(event), status: :ok
     else
-      render json: {status: :error, error: event.errors.messages}, status: 422
+      render json: {status: :error, error: event.errors.messages}, status: :unprocessable_entity
     end
   end
 
   def destroy
     @event.destroy
-    head(200)
+    head(:ok)
   end
 
   def update
     if @event.update_attributes(event_params)
-      render json: EventSerializer.new(@event), status: 201
+      render json: EventSerializer.new(@event), status: :created
     else
-      render json: {status: :error, error: @event.errors.messages}, status: 422
+      render json: {status: :error, error: @event.errors.messages}, status: :unprocessable_entity
     end
   end
 
