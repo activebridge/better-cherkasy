7b4637d04bf187ff360713e0ff825fc4657c24ca
Eugene Korpan <eugene@active-bridge.com>
2015-03-17 13:22:30 +0200

    Added number of comments for each event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authorize_user!, except: :index
+  before_action :authorize_user!, except: [ :index, :show ]
   before_action :find_own_event, only: :destroy
   before_action :find_event, only: :show
 
