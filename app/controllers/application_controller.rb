d243cd5738872f89051cd6d1eb0222b9dbdee461
Eugene Korpan <eugene@active-bridge.com>
2015-05-13 15:51:53 +0300

    Disable http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: 'active', password: 'bridge' if Rails.env.production?
+  #http_basic_authenticate_with name: 'active', password: 'bridge' if Rails.env.production?
 
   def current_user
     @user ||= User.find_by_auth_token(params[:auth_token])
